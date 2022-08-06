bb3d2340df0d0f142687e161f90760ec2a805156
yiyujin <yujin94@gmail.com>
2022-08-06 21:09:39 +0900

    image

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
       'www.notion.so',
       'images.unsplash.com',
       's3.us-west-2.amazonaws.com',
-      'https://s3.us-west-2.amazonaws.com',
     ],
     format: ['image/png', 'image/webp', 'image/jpeg']
   }
